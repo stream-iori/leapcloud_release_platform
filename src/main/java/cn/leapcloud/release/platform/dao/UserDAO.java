995b71755fa675869e05693310116d04ae39bdeb
song qian <sqcy2005@126.com>
2016-11-24 18:43:35 +0800

    finish dao in user

--- src/main/java/cn/leapcloud/release/platform/dao/UserDAO.java
+++ src/main/java/cn/leapcloud/release/platform/dao/UserDAO.java
@@ -1,7 +1,20 @@
 package cn.leapcloud.release.platform.dao;
 
+import cn.leapcloud.release.platform.service.domain.User;
+import org.jooq.Configuration;
+
 /**
  * Created by stream.
  */
 public interface UserDAO {
+
+  boolean doCreate(User user, Configuration configuration) throws Exception;
+
+  boolean doDelete(int id) throws Exception;
+
+  boolean doUpdate(User user) throws Exception;
+
+  User findById(int id) throws Exception;
+
+
 }
